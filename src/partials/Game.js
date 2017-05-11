3b81e4558dca1f065876076abc975dd0f618fcb2
heyliddle <eric.liddle@gmail.com>
2017-05-11 16:43:34 -0700

    THE PADDLES MOVE

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,3 +1,8 @@
+import { SVG_NS, KEYS } from '../settings';
+import Board from './Board';
+import Paddle from './Paddle';
+
+
 export default class Game {
 
 	constructor(element, width, height) {
@@ -5,11 +10,46 @@ export default class Game {
 		this.width = width;
 		this.height = height;
 	
-		this.gameElement = 
+		this.gameElement = document.getElementById(element);
+		
+		this.board = new Board(this.width, this.height);
+		this.paddleWidth = 8;
+		this.paddleHeight = 56;
+		this.padding = 10;
+		this.player1 = new Paddle(
+				height, 
+				this.paddleWidth,
+				this.paddleHeight,
+				this.padding,
+				(height-this.paddleHeight) / 2,
+				KEYS.a,
+				KEYS.z
+				);
+
+		this.player2 = new Paddle(
+				this.height,
+				this.paddleWidth,
+				this.paddleHeight,
+				(this.width - this.padding - this.paddleWidth),
+				(height-this.paddleHeight) / 2,
+				KEYS.up,
+				KEYS.down
+				);
 	}
 
 	render() {
-		// More code goes here...
+
+		this.gameElement.innerHTML = '';
+
+		let svg = document.createElementNS(SVG_NS, 'svg');
+		svg.setAttributeNS(null, 'width', this.width);
+		svg.setAttributeNS(null, 'height', this.height);
+		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${ this.height}`);
+		this.gameElement.appendChild(svg);
+
+		this.board.render(svg);
+		this.player1.render(svg);
+		this.player2.render(svg);
 	}
 
 }
---
+++ src/partials/Paddle.js
@@ -0,0 +1,47 @@
+import { SVG_NS } from '../settings';
+
+export default class Paddle {
+
+
+  constructor(boardHeight, width, height, x, y, up, down) {
+    this.boardHeight = boardHeight;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speed = 10;
+    this.score = 0;
+    
+
+    document.addEventListener('keydown', event => {
+      switch (event.key){
+        case up:
+          this.up();
+        break;
+        case down:
+          this.down();
+        break;
+      }
+    });
+  }
+
+  up () {
+    this.y = this.y - this.speed;
+
+  }
+
+down () {
+  this.y = this.y + this.speed;
+  }
+
+  render (svg) {
+    
+    const rect = document.createElementNS(SVG_NS, 'rect');
+    rect.setAttributeNS(null, 'x', this.x);
+    rect.setAttributeNS(null, 'y', this.y);
+    rect.setAttributeNS(null, 'width', this.width);
+    rect.setAttributeNS(null, 'height', this.height);
+    rect.setAttributeNS(null, 'fill', 'white');  
+    svg.appendChild(rect);
+  }
+}
